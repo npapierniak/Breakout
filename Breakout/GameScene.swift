1555537745331ac1b133fc47e9ebba8c0080ea06
Nicholas Papierniak <nick.papierniak@gmail.com>
2024-03-18 17:27:33 -0500

    Created brick object

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -11,10 +11,12 @@ import GameplayKit
 class GameScene: SKScene {
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
+    var brick = SKSpriteNode()
     override func didMove(to view: SKView) {
         createBackground()
         resetGame()
         makePaddle()
+        makeBrick()
     }
     func createBackground() {
         let stars = SKTexture (imageNamed: "Stars")
@@ -71,5 +73,15 @@ class GameScene: SKScene {
         paddle.physicsBody = SKPhysicsBody (rectangleOf: paddle.size)
         paddle.physicsBody?.isDynamic = false
         addChild(paddle)
+    }
+    func makeBrick() {
+        brick.removeFromParent ()
+        //remove the brick, if it exists
+        brick = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 20))
+        brick.position = CGPoint(x: frame.midX, y: frame.maxY - 50)
+        brick.name = "brick"
+        brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+        brick.physicsBody?.isDynamic = false
+        addChild(brick)
     }
 }
