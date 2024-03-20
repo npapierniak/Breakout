019e60f8563ed2032b10d7330f15e4ba5df8551b
Nicholas Papierniak <nick.papierniak@gmail.com>
2024-03-20 09:34:03 -0500

    Created brick ceiling

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -11,7 +11,7 @@ import GameplayKit
 class GameScene: SKScene, SKPhysicsContactDelegate {
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
-    var brick = SKSpriteNode()
+    var bricks = [SKSpriteNode]()
     var loseZone = SKSpriteNode()
     var playLabel = SKLabelNode ()
     var livesLabel = SKLabelNode ()
@@ -19,6 +19,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var playingGame = false
     var score = 0
     var lives = 3
+    var removedBricks = 0
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
         self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
@@ -26,7 +27,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         resetGame()
         updateLabels()
         makePaddle()
-        makeBrick()
+        makeBricks()
         makeLoseZone()
         makeLabels()
     }
@@ -93,16 +94,65 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         paddle.physicsBody?.isDynamic = false
         addChild(paddle)
     }
-    func makeBrick() {
-        brick.removeFromParent ()
-        //remove the brick, if it exists
-        brick = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 20))
-        brick.position = CGPoint(x: frame.midX, y: frame.maxY - 50)
-        brick.name = "brick"
+    func makeBrick(x: Int, y: Int, color: UIColor) {
+        let brick = SKSpriteNode (color: color, size: CGSize(width: 50, height: 20))
+        brick.position = CGPoint (x: x, y: y)
         brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
         brick.physicsBody?.isDynamic = false
         addChild(brick)
+        bricks.append(brick)
     }
+    func makeBricks() {
+        // first, remove any leftover bricks (from prior game)
+        for brick in bricks {
+            if brick.parent != nil {
+                brick.removeFromParent ()
+            }
+        }
+        bricks.removeAll ()
+        removedBricks = 0
+        //clear the array
+        // reset the counter
+        //now, figure the number and spacing of each row of bricks
+        let count = Int(frame.width) / 55 // bricks per row
+        let xOffset = (Int(frame.width) - (count * 55)) / 2 + Int (frame.minX) + 25
+        let y = Int (frame.maxY) - 65
+        for i in 0..<count {
+            let x = i * 55 + xOffset
+            makeBrick(x: x, y: y, color: .green)
+        }
+    }
+    func didBegin(_ contact: SKPhysicsContact) {
+        //ask each brick, "Is it you?'
+        for brick in bricks {
+            if contact.bodyA.node == brick ||
+                contact.bodyB.node == brick
+            {
+                score += 1
+                updateLabels ()
+                brick.removeFromParent ()
+                removedBricks += 1
+                if removedBricks == bricks.count {
+                    gameOver(winner: true)
+                }
+            }
+        }
+        if contact.bodyA.node?.name ==
+            "loseZone" ||
+            contact.bodyB.node?.name == "tosezone"{
+            lives -= 1
+            if lives > 0 {
+                score = 0
+                resetGame()
+                kickBall()
+            }
+            else{
+                gameOver(winner: false)
+            }
+        }
+    }
+    
+    
     func makeLoseZone() {
         loseZone = SKSpriteNode(color: .red, size: CGSize(width: frame.width, height: 50))
         loseZone.position = CGPoint(x: frame.midX, y: frame.minY + 25)
@@ -159,15 +209,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             if playingGame {
                 paddle.position.x = location.x
             }
-        }
-    }
-    func didBegin(_ contact: SKPhysicsContact) {
-        if contact.bodyA.node?.name == "brick" || contact.bodyB.node?.name == "brick" {
-            gameOver (winner: true)
-        }
-        if contact.bodyA.node?.name == "loseZone" ||
-            contact.bodyB.node?.name == "loseZone"{
-            gameOver(winner: false)
         }
     }
     func gameOver (winner: Bool) {
